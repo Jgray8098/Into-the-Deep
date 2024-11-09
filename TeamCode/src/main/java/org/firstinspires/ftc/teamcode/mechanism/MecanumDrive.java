69f7af5d7f68653a146fb9a11707d8612ebd265d
Jeremy Gray <whextra330@yahoo.com>
2024-11-09 16:52:21 -0600

    Initial commit for Cyclops code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanism/MecanumDrive.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.mechanism;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class MecanumDrive {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor backLeftMotor;
+    private DcMotor backRightMotor;
+
+    public void init(HardwareMap hardwareMap) {
+        frontLeftMotor = hardwareMap.dcMotor.get("LeftFrontMotor");
+        frontRightMotor = hardwareMap.dcMotor.get("RightFrontMotor");
+        backLeftMotor = hardwareMap.dcMotor.get("LeftBackMotor");
+        backRightMotor = hardwareMap.dcMotor.get("RightBackMotor");
+
+        //frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    private void setPowers(double frontLeftPower, double frontRightPower, double
+                           backLeftPower, double backRightPower) {
+        double maxSpeed = 1.0;
+        maxSpeed = Math.max(maxSpeed, Math.abs(frontLeftPower));
+        maxSpeed = Math.max(maxSpeed, Math.abs(frontRightPower));
+        maxSpeed = Math.max(maxSpeed, Math.abs(backLeftPower));
+        maxSpeed = Math.max(maxSpeed, Math.abs(backRightPower));
+
+        frontLeftPower /= maxSpeed;
+        frontRightPower /= maxSpeed;
+
+        backLeftPower /= maxSpeed;
+        backRightPower /= maxSpeed;
+
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+        backLeftMotor.setPower(backLeftPower);
+        backRightMotor.setPower(backRightPower);
+    }
+
+    public void drive(double forward, double right, double rotate) {
+        double frontLeftPower = forward + right + rotate;
+        double frontRightPower = forward - right - rotate;
+        double backLeftPower = forward - right + rotate;
+        double backRightPower = forward + right - rotate;
+
+        setPowers(frontLeftPower, frontRightPower, backLeftPower, backRightPower);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MecanumDriveMode.java
@@ -0,0 +1,66 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.mechanism.MecanumDrive;
+
+@TeleOp()
+public class MecanumDriveMode extends OpMode {
+    MecanumDrive drive = new MecanumDrive();
+    //private DcMotor IntakeMotorRight;
+    //private DcMotor IntakeMotorLeft;
+   // private DcMotor LiftMotor;
+    //int liftUpPosition = -2800;
+    //int liftDownPosition = 0;
+
+    @Override
+    public void init() {
+        drive.init(hardwareMap);
+        //IntakeMotorRight = hardwareMap.dcMotor.get("IntakeMotorRight");
+        //IntakeMotorLeft = hardwareMap.dcMotor.get("IntakeMotorLeft");
+       // LiftMotor = hardwareMap.dcMotor.get("LiftMotor");
+        //LiftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //LiftMotor.setTargetPosition(liftDownPosition);
+        //LiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+    }
+
+    @Override
+    public void loop() {
+        //Mecanum Drive Program
+        double forward = -gamepad1.right_stick_y;
+        double right = gamepad1.right_stick_x;
+        double rotate = gamepad1.left_stick_x;
+
+        drive.drive(forward, right, rotate);
+
+        //Intake Program
+        //if (gamepad1.a) {
+         //   IntakeMotorRight.setPower(1.0);
+         //   IntakeMotorLeft.setPower(-1.0);
+       // }
+        //IntakeMotorRight.setPower(0);
+        //IntakeMotorLeft.setPower(0);
+
+        //Lift Program
+        //if (gamepad1.y) {
+         //   LiftMotor.setTargetPosition(liftUpPosition);
+          //  LiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+         //   LiftMotor.setPower(-0.5);
+       // }
+       // if(gamepad1.b) {
+           // LiftMotor.setTargetPosition(liftDownPosition);
+            //LiftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //LiftMotor.setPower(0.5);
+       // }
+        //double position = LiftMotor.getCurrentPosition();
+       // double desiredPosition = LiftMotor.getTargetPosition();
+       // telemetry.addData("Encoder Position",position);
+       // telemetry.addData("Desired Position",desiredPosition);
+       // telemetry.update();
+        //Gripper Program
+    }
+
+    }
